c2bfa0c6af84fa35776a181f14538e3667e8d0bc
PatrickZambrano <75545478+PatrickZambrano@users.noreply.github.com>
2022-02-19 12:34:19 -0800

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Patrick Zambrano on 19.02.22.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
